e58aa4443a5a9f1b8b60e09ebeadb11a32a005d2
James <37276661+capjamesg@users.noreply.github.com>
2024-05-05 19:30:30 +0100

    update to work with new openai website

--- app.py
+++ app.py
@@ -2,37 +2,62 @@ import datetime
 import os
 
 import bs4
-import requests
 from feedgen.feed import FeedGenerator
+from playwright.sync_api import sync_playwright
 
 HOME_DIR = "/var/www/jamesg.blog/"
 
-url = "https://openai.com/blog"
+url = "https://openai.com/news"
 
-page = requests.get(url)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
+}
 
-soup = bs4.BeautifulSoup(page.content, "html.parser")
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
+    page.set_extra_http_headers(headers)
+    page.goto(url)
+    html = page.content()
+    browser.close()
 
-# traverse whole tree of A tags
+soup = bs4.BeautifulSoup(html, "html.parser")
+
 urls = {}
 
 for a in soup.find_all("a"):
-    # if any child is h3, print it
-    # get sr-only
-    if a.find("span", class_="sr-only"):
-        date = a.find("span", class_="sr-only").text
-        # May 18, 2023
-        date = datetime.datetime.strptime(date, "%B %d, %Y")
-        # add PT timezone
-        date = date.strftime("%Y-%m-%dT%H:%M:%S") + "-08:00"
-    else:
-        date = None
+    
+    if not a.has_attr("href"):
+        continue
 
-    if a.find("h3"):
-        urls[a.find("h3").text] = {
-            "url": "https://openai.com" + a["href"],
-            "date": date,
-        }
+    if not a["href"].startswith("/index"):
+        continue
+
+    title = a.get("aria-label", None)
+
+    if title is None:
+        continue
+
+    date = None
+
+    for span in a.find_all("span"):
+        if span.has_attr("class") and "text-small" in span["class"]:
+            try:
+                date = datetime.datetime.strptime(span.text, "%b %d, %Y")
+            except ValueError:
+                pass
+
+    if date is None:
+        continue
+
+    date = date.replace(tzinfo=datetime.timezone.utc)
+
+    urls[title] = {
+        "url": "https://openai.com" + a["href"],
+        "date": date,
+    }
+
+urls = {url: urls[url] for url in urls if urls[url]["date"] is not None}
 
 fg = FeedGenerator()
 fg.id("https://openai.com/blog")
@@ -43,7 +68,6 @@ fg.logo("https://openai.com/favicon.ico")
 fg.subtitle("OpenAI Blog posts.")
 fg.link(href="https://jamesg.blog/openai.xml", rel="self")
 
-# order urls by date
 urls = dict(sorted(urls.items(), key=lambda item: item[1]["date"], reverse=False))
 
 for link in urls:
